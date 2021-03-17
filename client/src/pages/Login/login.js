34f4cca7770990e115bdabe96cc369a529b18007
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-03-17 16:05:20 -0400

    page setup

---
+++ client/src/pages/Login/login.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './login.css'
+import { SiLivejournal } from 'react-icons/si'
+import { FcGoogle } from 'react-icons/fc'
+// import { Link } from 'react-router-dom'
+// import LogoutBtn from '../../components/LogoutBtn'
+// import LoginBtn from '../../components/LoginBtn'
+
+const login = () => {
+    return (
+        <>
+        </>
+        // <div className="container login-container">
+        //     <div className="card pb-5" >
+        //         <div className="card-content">
+        //             <h1 > Personalized Journal</h1>
+        //             <SiLivejournal size={'50%'} />
+        //             <div className="section mt-5">
+        //                 <p className="lead">Begin your Journey!!!!!!</p>
+        //             </div>
+        //             <div className="divider mt-2"></div>
+        //             <div className="section" style={{ overflow: 'clear' }}>
+        //                 <FcGoogle size={'50%'} />
+        //                 {/* <Link to="/auth/google" className="btn red darken-1"> */}
+        //                 <LoginBtn />
+        //                 {/* <span style={{ backgroundColor: 'red', color: 'white', padding: 12, borderRadius: 30 }}>Log In With Google</span> */}
+        //                 {/* </Link> */}
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+    )
+}
+
+export default login
