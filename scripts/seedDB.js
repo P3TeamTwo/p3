abc723af3e35350b5df79e1f6ee772f922ace0c7
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-18 11:58:29 -0400

    replacing the seeder file with local db connection information

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,31 +1,44 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
+// This file empties the journal collection and inserts the books below
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/reactreadinglist"
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/journal"
 );
 
-const bookSeed = [
-  {
-    title: "The Dead Zone",
-    author: "Stephen King",
-    synopsis:
-      "A number-one national best seller about a man who wakes up from a five-year coma able to see people's futures and the terrible fate awaiting mankind in The Dead Zone - a \"compulsive page-turner\" (The Atlanta Journal-Constitution). Johnny Smith awakens from a five-year coma after his car accident and discovers that he can see people's futures and pasts when he touches them. Many consider his talent a gift; Johnny feels cursed. His fiancée married another man during his coma, and people clamor for him to solve their problems. When Johnny has a disturbing vision after he shakes the hand of an ambitious and amoral politician, he must decide if he should take drastic action to change the future. The Dead Zone is a \"faultlessly paced...continuously engrossing\" (Los Angeles Times) novel of second sight.",
-    date: new Date(Date.now())
-  }
+const journalSeed = [
+    {
+        title: "Tuesday 16th March",
+        body: "Today was a really good day",
+        array: [{
+            would: "we",
+            store: "answers",
+            as: "key",
+            value: "pairs?"
+        }]
+    },
+    {
+        title: "Tuesday 15th March",
+        body: "Today was also an amazing day",
+        array: [{
+            would: "we",
+            store: "answers",
+            as: "key",
+            value: "pairs?"
+        }]
+    }
 ];
 
-db.Book
-  .remove({})
-  .then(() => db.Book.collection.insertMany(bookSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+db.JournalEntry
+    .remove({})
+    .then(() => db.JournalEntry.collection.insertMany(journalSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
