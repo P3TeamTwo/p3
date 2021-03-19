8bb44046e3f71186899975fb58d5edb45b69e49d
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-18 22:17:41 -0400

    adding question rotation

--- client/src/pages/DailyReflection/DailyReflection.js
+++ client/src/pages/DailyReflection/DailyReflection.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './DailyReflection.css';
 // Material UI imports
-import { Grid, Paper, makeStyles, TextField } from '@material-ui/core';
+import { Grid, Paper, makeStyles, TextField, FormControl, RadioGroup, FormLabel, FormControlLabel, Radio, Button } from '@material-ui/core';
 // Import Navbar
 // import Navbar from '../../components/Navbar';
 // Import mood slider
 import MoodSlider from '../../components/MoodSlider';
 //Importing Radio button style quesetion format
-import RadioBtnQFormat from '../../components/RadioBtnQFormat'
+// import RadioBtnQFormat from '../../components/RadioBtnQFormat'
 //importing long form style question format 
-import LongForm from '../../components/LongFormQFormat'
+// import LongForm from '../../components/LongFormQFormat'
+//importing questions
+import questions from "../../questions/Questions.json"
+
 const DailyReflection = () => {
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -21,14 +24,32 @@ const DailyReflection = () => {
       color: theme.palette.text.secondary,
     },
   }));
+
+  const questionTotal = 5
+  console.log(questions)
+
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+
+  const [radioValue, setRadioValue] = useState('');
+
+  const handleChange = (event) => {
+    setRadioValue(event.target.value);
+  };
+  
   // Function that will kick off the questions
   function startQuestions() {
     console.log("Mood slider submit has been clicked")
-    const questionTotal = 5
   }
   // Function to invoke the next question
   function nextQuestion() {
     console.log("Radio question submit has been clicked")
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questionTotal) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      console.log("End of Questions - show dashboard")
+    }
+
   }
   const classes = useStyles();
   return (
@@ -42,24 +63,40 @@ const DailyReflection = () => {
               <b>Daily Reflection</b>
               <p>How are you feeling today?</p>
               {/* Importing the mood slider */}
-              <MoodSlider 
-              // Start questions function will be called in mood slider
-              handleClick={startQuestions}
+              <MoodSlider
+                // Start questions function will be called in mood slider
+                handleClick={startQuestions}
               />
             </Paper>
             <Paper className={classes.paper}>
               <b>Radio buttons</b>
               {/* Importing the mood slider */}
-              <RadioBtnQFormat 
+              {/* <RadioBtnQFormat 
               handleClick={nextQuestion}
-              />
+              /> */}
+
+
+              <FormControl component="fieldset">
+                <FormLabel component="legend">{questions[currentQuestion].questionText}</FormLabel>
+                {/* removed value and onChange below */}
+                <RadioGroup aria-label="gender" name="gender1" value={radioValue} onChange={handleChange}>
+                  {questions[currentQuestion].answerOptions.map((answerOption) => {
+                    console.log(answerOption)
+                    return (
+                    <FormControlLabel key={questions.id} value={answerOption} control={<Radio />} label={answerOption} />
+                    )
+                  })}
+                </RadioGroup>
+
+                <Button variant="outlined" onClick={nextQuestion}>SUBMIT</Button>
+              </FormControl>
             </Paper>
           </Grid>
           {/* COLUMN */}
           {/* <Grid className='gridPadding' item xs={12} sm={6}>
             <Paper className={classes.paper}> */}
-              {/* FORM */}
-              {/* <LongForm />
+          {/* FORM */}
+          {/* <LongForm />
             </Paper>
           </Grid> */}
         </Grid>
