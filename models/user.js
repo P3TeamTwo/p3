d10e5a339b62d37a4a7366b40b421c16f482dc03
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-03-20 13:11:11 -0400

    test

--- models/user.js
+++ models/user.js
@@ -2,25 +2,39 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-    name: {
+    username: {
         type: String,
         required: true,
         min: 6,
         max: 15
     },
-    email: {
-        type: String,
-        required: true
-    },
-
     password: {
         type: String,
         required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
     }
 });
+
+UserSchema.pre('save', function (next) {
+    if (!this.isModified('password'))
+        return next();
+    bcrypt.hash(this.password, 10, (err, passwordHash) => {
+        if (err)
+            return next(err);
+        this.password = passwordHash;
+        next();
+    });
+});
+
+UserSchema.methods.comparePassword - function (password, cb) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if (err)
+            return cb(err);
+        else {
+            if (!isMatch)
+                return cb(null, isMatch);
+                return cb(null,this);
+        }
+    });
+}
 
 module.exports = mongoose.model('User', UserSchema)
