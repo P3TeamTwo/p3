d5a269a386d0c975204a678b86da70cb3abfcbcb
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-03-20 07:49:27 -0400

    ..

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
-const path = require("path");
-const router = require("express").Router();
-const apiRoutes = require("./api");
+// const path = require("path");
+// const router = require("express").Router();
+// const apiRoutes = require("./api");
+// const userRoutes = require("./user");
 
-// API Routes
-router.use("/api", apiRoutes);
+// // API Routes
+// router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// //User
 
-module.exports = router;
+// router.use("/user", userRoutes);
+
+// // If no API routes are hit, send the React app
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,40 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
+
+
+// // Add routes, both API and view
+// const routes = require("./routes");
+// app.use(routes);
+// Login
+const userRouter = require("routes/User");
+app.use('/user', userRouter);
 
 // How to connect locally "mongodb://localhost/YOUR DATA BASE NAME"
 mongoose.connect(
   process.env.MONGDB_URI || 'mongodb://localhost/journal', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-  }
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
 );
 
+
+
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
