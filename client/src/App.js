45c75851236cc119e7bcfd4371fd135659a9fb8f
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2021-03-20 10:38:51 -0400

    updated validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import PrivateRoute from '../src/components/PrivateRoute';
-import { useHistory } from "../src/components/LoginBtn";
+import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 import Homepage from './pages/Homepage/homepage';
 import Welcome from './pages/Welcome/index.js';
@@ -17,18 +16,16 @@ function App() {
 
 
   return (
-    <useHistory value={false}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/Daily" component={DailyReflection} />
-          <PrivateRoute exact path='/welcome' component={Welcome} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path='*' component={NoMatch} />
-        </Switch>
-      </Router>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <ProtectedRoute exact path="/Daily" component={DailyReflection} />
+        <ProtectedRoute exact path='/welcome' component={Welcome} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path='*' component={NoMatch} />
+      </Switch>
+    </Router>
 
-    </useHistory>
 
 
   );
---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+
+class ProtectedRoute extends React.Component {
+
+    render() {
+        const Component = this.props.component;
+        const isAuthenticated = localStorage.getItem('token');
+
+        return isAuthenticated ? (
+            <Component />
+        ) : (
+            <Redirect to={{ pathname: '/login' }} />
+        );
+    }
+}
+
+export default ProtectedRoute;
