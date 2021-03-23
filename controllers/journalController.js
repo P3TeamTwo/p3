957de7ec5167fc851e87b400cdda0d1bb507aeec
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-22 20:55:44 -0400

    db connection

--- controllers/journalController.js
+++ controllers/journalController.js
@@ -9,12 +9,12 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findById: function(req, res) {
-    db.JournalEntry
-      .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
+  // findById: function(req, res) {
+  //   db.JournalEntry
+  //     .findById(req.params.id)
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
   create: function(req, res) {
     db.JournalEntry
       .create(req.body)
