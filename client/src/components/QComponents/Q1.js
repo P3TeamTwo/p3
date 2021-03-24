c447cb6ce284d23277727e9657580d59ea2c26c9
Hustin <hustinkava@gmail.com>
2021-03-23 23:41:14 -0600

    question logic complete

--- client/src/components/QComponents/Q1.js
+++ client/src/components/QComponents/Q1.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import questions from '../../Questions.json';
 import { FormControl, RadioGroup, FormLabel, FormControlLabel, Radio } from '@material-ui/core';
-import Q1questions from '../../Q1.json'
+import Q1questions from '../Questions/Q1.json';
+
 function Q1(props) {
-    // const storeQuestions = Q1questions[0]
+
     const randomQuestion = Q1questions[Math.floor(Q1questions.length * Math.random())];
-    // console.log(randomQuestion.question)
+
     return(
         <>
         <b>Question 1</b>
@@ -13,24 +13,23 @@ function Q1(props) {
             <FormControl component="fieldset">
                 <FormLabel component="legend">{randomQuestion.question}
                 </FormLabel>
-                <RadioGroup 
-                defaultValue='false' 
-                aria-label="question1" name="question1" 
-                value='question1'
-                onChange={(e)=> props.handleSubmit(e, null, e.target.value, e.target)}> 
                 {randomQuestion.answers.map(answer => {
                     return (
+                        <RadioGroup 
+                        key={answer.answerText}
+                        defaultValue='false' 
+                        aria-label="question1" name={answer.test} 
+                        value='question1'
+                        onChange={(e)=> props.handleSubmit(e, null, e.target.value, e.target.name)}> 
                         <FormControlLabel
-                        key={answer.answerText} 
+                         
                         value={answer.value} 
                         control={<Radio />}
                         label={answer.answerText}
-                        id={answer.id} />
+                        data-tag={answer.test} />
+                </RadioGroup>
                     )
                 })}
-                {/* <FormControlLabel value="true" control={<Radio />} label="true" />
-                <FormControlLabel value="false" control={<Radio />} label="false" /> */}
-                </RadioGroup>
             </FormControl>
         </div>
         </>  
