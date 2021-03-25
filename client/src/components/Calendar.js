bbc8a73f05490283d4cea7694c78d612b353651a
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2021-03-25 14:33:07 -0400

    Calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,21 +1,52 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React from 'react';
+import Calendar from 'react-awesome-calendar';
+import styles from '../components/calendar.css';
 
-function MyApp() {
-    const [value, onChange] = useState(new Date());
+const events = [{
+    id: 1,
+    color: '#008000',
+    from: '2021-03-02T18:00:00+00:00',
+    to: '2021-03-07T19:00:00+00:00',
+    title: 'Happy',
+}, {
+    id: 2,
+    color: '#0000FF',
+    from: '2021-03-08T13:00:00+00:00',
+    to: '2021-03-10T14:00:00+00:00',
+    title: 'Sad',
+}, {
+    id: 3,
+    color: '#F480A8',
+    from: '2021-03-T13:00:00+00:00',
+    from: '2020-11-05T00:00:00+00:00',
+    to: '2020-11-06T00:01:00+00:00',
+    title: 'Okay',
+}]
 
-    return (
-        <div>
-            <Calendar
-                onChange={onChange}
-                value={value}
+class CalApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.calendar = React.createRef();
+    }
 
+    componentDidMount() {
+        const details = this.calendar.current.getDetails();
+        // call get events endpoint
+    }
 
-
-            />
-        </div>
-    );
+    render() {
+        return (
+            <div className={styles.pageCalendar}>
+                <Calendar
+                    ref={this.calendar}
+                    onClickEvent={(event) => console.log('this is an event', event)}
+                    onChange={(dates) => localStorage.setItem('dates', dates)}
+                    onClickTimeLine={(date) => localStorage.setItem('date', date)}
+                    events={events}
+                />
+            </div>
+        );
+    }
 }
 
-export default MyApp
+export default CalApp;
