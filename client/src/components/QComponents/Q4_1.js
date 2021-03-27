900a3de374f1039a434f6b7adb1273ec91c46193
Keith Crooc <keith.crooc@gmail.com>
2021-03-26 23:16:08 -0400

    working out long form for social questions

--- client/src/components/QComponents/Q4_1.js
+++ client/src/components/QComponents/Q4_1.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { FormControl, RadioGroup, FormLabel, FormControlLabel, Radio } from '@material-ui/core';
+import React, { useState } from 'react';
+import { FormControl, RadioGroup, FormLabel, FormControlLabel, Radio, TextField, Button } from '@material-ui/core';
 import Q4questions from '../Questions/Q4.json';
 
 function Q4_1(props) {
 
     console.log(Q4questions[0].question)
+
+    const [userResponse, setUserResponse] = useState();
+
+    const handleChange = (e) => {
+        setUserResponse(e.target.value);
+    }
+
 
     return(
         <>
@@ -12,23 +19,28 @@ function Q4_1(props) {
             <div className='container'>
             <FormControl component="fieldset">
                 <FormLabel component="legend">{Q4questions[0].question}
+                
+                <TextField
+                id="outlined-multiline-static"
+                label={Q4questions}
+                multiline
+                rows={4}
+                variant="outlined"
+                style={{ minWidth: "100%" }}
+                onChange={(e) => {
+                    handleChange(e)
+                }}
+            />
+            <Button variant="contained"
+                onClick={(e) => {
+                    console.log(userResponse)
+                    props.handleSubmit(e, userResponse)
+                }
+                }>SUBMIT</Button>
+                
+                
                 </FormLabel>
-                {Q4questions[0].answers.map(answer => {
-                    return (
-                        <RadioGroup 
-                        key={answer.answerText}
-                        defaultValue='false' 
-                        aria-label="question4_1" name={answer.test} 
-                        value='question4_1'
-                        onChange={(e)=> props.handleSubmit(e, null, null, null, null, null, null, null, null, null, null, e.target.value)}> 
-                        <FormControlLabel
-                         
-                        value={answer.value} 
-                        control={<Radio />}
-                        label={answer.answerText}/>
-                </RadioGroup>
-                    )
-                })}
+                
             </FormControl>
         </div>
         </>  
