4a6bcf1c95391e7390686f291c611b2530ef597f
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-28 11:47:39 -0400

    changing longform question to not required - no validation and allows next questio

--- client/src/pages/Graph/Graphs.js
+++ client/src/pages/Graph/Graphs.js
@@ -58,7 +58,7 @@
 
 // export default Graphs;
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
@@ -79,125 +79,126 @@ import './Graph.css'
 
 function TabPanel(props) {
 
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography component={'span'}>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography component={'span'}>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
 TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
 };
 
 function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
-  };
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-    display: 'flex',
-    height: 224,
-  },
-  tabs: {
-    borderRight: `1px solid ${theme.palette.divider}`,
-  },
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        height: 224,
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
 }));
 
 export default function VerticalTabs() {
 
 
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
 
-  //Getting the user ID
-  const userId = localStorage.getItem("userId");
+    //Getting the user ID
+    const userId = localStorage.getItem("userId");
 
-  // Setting state to store the journl entries
-const [entries, setEntries] = useState();
+    // Setting state to store the journl entries
+    const [entries, setEntries] = useState();
 
-useEffect(() => {
-  //Get all journal data for the user logged in 
-  API.getJournal(userId)
-      .then(res => {
-          setEntries(res.data)
-      }
-      )
-      .catch(err => console.log(err))
+    useEffect(() => {
+        //Get all journal data for the user logged in 
+        API.getJournal(userId)
+            .then(res => {
+                setEntries(res.data)
+            }
+            )
+            .catch(err => console.log(err))
 
-}, [])
+    }, [])
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-console.log(entries)
-  return (
-    <div className={classes.root}>
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        className={classes.tabs}
-      >
-        <Tab label="Sleep" {...a11yProps(0)} />
-        <Tab label="Exercise" {...a11yProps(1)} />
-        <Tab label="Eating Habits" {...a11yProps(2)} />
-        <Tab label="Social" {...a11yProps(3)} />
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    
+    console.log(entries)
+    return (
+        <div className={classes.root}>
+            <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                className={classes.tabs}
+            >
+                <Tab label="Sleep" {...a11yProps(0)} />
+                <Tab label="Exercise" {...a11yProps(1)} />
+                <Tab label="Eating Habits" {...a11yProps(2)} />
+                <Tab label="Social" {...a11yProps(3)} />
 
-      </Tabs>
-      <TabPanel value={value} index={0} >
-            {/* once entries has value and linegraph can access values then execute */}
-            {entries && <HoursOfSleep
-                // set the prop dates as a map of the entries, taking the date entered and the data poitns from q1_
-                dates={entries.map(entry => ({ date: entry.created_at, point: entry.q1_1 }))}
-            />}
+            </Tabs>
+            <TabPanel value={value} index={0} >
+                {/* once entries has value and linegraph can access values then execute */}
+                {entries && <HoursOfSleep
+                    // set the prop dates as a map of the entries, taking the date entered and the data poitns from q1_
+                    dates={entries.map(entry => ({ date: entry.created_at, point: entry.q1_1 }))}
+                />}
 
-            {/* Coffee vs sleep double axis line graph */}
-            {entries && <CoffeeVsSleep
-                datesAndSleep={entries.map(entry => ({ date: entry.created_at, point: entry.q1_1 }))}
-                datesAndCoffee={entries.map(entry => ({ date: entry.created_at, point: entry.q1_3 }))}
-            />}
+                {/* Coffee vs sleep double axis line graph */}
+                {entries && <CoffeeVsSleep
+                    datesAndSleep={entries.map(entry => ({ date: entry.created_at, point: entry.q1_1 }))}
+                    datesAndCoffee={entries.map(entry => ({ date: entry.created_at, point: entry.q1_3 }))}
+                />}
 
-            {/* Screen time doughnut graph display */}
-            {/* {entries && <ScreenTimeDoughnut
+                {/* Screen time doughnut graph display */}
+                {/* {entries && <ScreenTimeDoughnut
                 screenTimeNights={entries.map(entry => ({ date: entry.created_at, point: entry.q1_2 }))}
 
             />} */}
 
-            {/* <ScreenVsSleep /> */}
-</TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two - insert graph code here for exercise
+                {/* <ScreenVsSleep /> */}
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two - insert graph code here for exercise
       </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three - insert graph code here for eating habits 
+            <TabPanel value={value} index={2}>
+                Item Three - insert graph code here for eating habits
       </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item four - insert graph code here for social 
+            <TabPanel value={value} index={3}>
+                Item four - insert graph code here for social
       </TabPanel>
 
-    </div>
-  );
+        </div>
+    );
 }
 
--- models/journalModel.js
+++ models/journalModel.js
@@ -54,19 +54,19 @@ const journalEntrySchema = new Schema({
   },
   q4_1: {
     type: String,
-    required: true
+    required: false
   },
   q4_2: {
     type: String,
-    required: true
+    required: false
   },
   q4_3: {
     type: String,
-    required: true
+    required: false
   },
   longForm: {
     type: String,
-    required: true
+    required: false
   },
   voiceMemo: {
     type: String,
