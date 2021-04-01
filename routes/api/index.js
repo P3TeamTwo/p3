688037ceec69144b0004d8f5eadef00e830ae5d9
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-03-31 21:52:51 -0400

    pull LongForm

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,14 +3,11 @@ const path = require("path");
 
 const journalRoutes = require("./journal");
 const userRoutes = require("./user");
-const filesRoutes = require("./files")
 
 // journal routes
 router.use("/journal", journalRoutes);
 // Users
 router.use("/user", userRoutes);
-// Memos
-router.use("/files", filesRoutes)
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
