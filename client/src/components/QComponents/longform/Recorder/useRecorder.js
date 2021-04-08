d7adf85f886cd14e63393fdb4bc937f240a1b4f7
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-04-08 18:56:41 -0400

    s3 incomplete

--- client/src/components/QComponents/longform/Recorder/useRecorder.js
+++ client/src/components/QComponents/longform/Recorder/useRecorder.js
@@ -1,51 +1,67 @@
 import { useEffect, useState } from "react";
+import ReactS3 from "react-s3";
+
+
+// //Optional Import
+// import { uploadFile } from 'react-s3';
+
+const config = {
+    bucketName: 'voice-note',
+    dirName: 'memos',
+    region: 'ca-central-1',
+    accessKeyId: 'AKIAWNFFL3CWKUZWFZ5L',
+    secretAccessKey: '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',
+}
 
 const useRecorder = () => {
-  const [audioURL, setAudioURL] = useState("");
-  const [isRecording, setIsRecording] = useState(false);
-  const [recorder, setRecorder] = useState(null);
+    const [audioURL, setAudioURL] = useState("");
+    const [isRecording, setIsRecording] = useState(false);
+    const [recorder, setRecorder] = useState(null);
 
 
 
-  useEffect(() => {
+    useEffect(() => {
 
-    if (recorder === null) {
-      if (isRecording) {
-        requestRecorder().then(setRecorder, console.error);
-      }
-      return;
-    }
+        if (recorder === null) {
+            if (isRecording) {
+                requestRecorder().then(setRecorder, console.error);
+            }
+            return;
+        }
 
-    // Manage recorder state.
-    if (isRecording) {
-      recorder.start();
-    } else {
-      recorder.stop();
-    }
+        // Manage recorder state.
+        if (isRecording) {
+            recorder.start();
+        } else {
+            recorder.stop();
+        }
 
 
-    // Obtain the audio when ready.
-    const handleData = e => {
-      setAudioURL(URL.createObjectURL(e.data));
+        // Obtain the audio when ready.
+        const handleData = e => {
+            setAudioURL(URL.createObjectURL(e.data));
+            ReactS3.upload(e.data, config)
+                .then(data => console.log(data))
+                .catch(err => console.error(err))
+        };
+
+        recorder.addEventListener("dataavailable", handleData);
+        return () => recorder.removeEventListener("dataavailable", handleData);
+    }, [recorder, isRecording]);
+
+    const startRecording = () => {
+        setIsRecording(true);
     };
 
-    recorder.addEventListener("dataavailable", handleData);
-    return () => recorder.removeEventListener("dataavailable", handleData);
-  }, [recorder, isRecording]);
-
-  const startRecording = () => {
-    setIsRecording(true);
-  };
-
-  const stopRecording = () => {
-    setIsRecording(false);
-  };
-  localStorage.setItem('voice', audioURL);
-  return [audioURL, isRecording, startRecording, stopRecording];
+    const stopRecording = () => {
+        setIsRecording(false);
+    };
+    localStorage.setItem('voice', audioURL);
+    return [audioURL, isRecording, startRecording, stopRecording];
 };
 
 async function requestRecorder() {
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-  return new MediaRecorder(stream);
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    return new MediaRecorder(stream);
 }
 export default useRecorder;
