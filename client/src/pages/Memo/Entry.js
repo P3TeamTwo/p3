705f2ed91ffc58e22ffc53b7c799ffd62171d111
Keith Crooc <keith.crooc@gmail.com>
2021-04-13 16:29:05 -0400

    when you delete an entry, the page will reload to sho show entry has been deleted

--- client/src/pages/Memo/Entry.js
+++ client/src/pages/Memo/Entry.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from 'react-router-dom'
 // import Player from '../../components/QComponents/longform/Recorder/Player'
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import Player from "../../components/QComponents/longform/Recorder/Player";
 import {Button} from '@material-ui/core'
 
+
 const Entry = ({ memo, deleteEntry }) => {
+
+    
 
     sessionStorage.removeItem('AI_sentBuffer');
     sessionStorage.removeItem('AI_buffer');
     const audioURL = `https://voice-note.s3.ca-central-1.amazonaws.com/` + `${memo.created_at}-${memo.postedBy}` + `/undefined`;
+    console.log(memo)
     console.log(audioURL);
     return (
         <Card variant="outlined" style={{ marginBottom: '2%', padding: '1%' }}>
