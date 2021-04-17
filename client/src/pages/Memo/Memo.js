1e072d11230c76e00d9160150c158c558e340eba
Hustin <hustinkava@gmail.com>
2021-04-16 18:03:59 -0600

    github pages

--- client/src/pages/Memo/Memo.js
+++ client/src/pages/Memo/Memo.js
@@ -1,29 +1,50 @@
 import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-
+import Button from '@material-ui/core/Button'
 import API from '../../utils/API'
-
+//importing context
+import dateContext from '../../utils/DateContext'
 // import WordMap from './WordMap'
 import Memos from './Memos'
 import '../Graph/Graph.css'
-
+import { makeStyles } from '@material-ui/core/styles'
+const useStyle = makeStyles({
+    backdateButton: {
+        margin: "10px auto 0 auto",
+        width: '100%',
+        borderRadius: '15px',
+        backgroundColor: '#d8ac9c',
+        color: 'black',
+        fontSize: '20px',
+        display: 'block',
+        "&:hover": {
+            backgroundColor: '#7eca9c',
+            color: 'white'
+        },
+        boxShadow: '0 9px 30px -5px #a6aa9c'
+    }
+})
 const Journal = ({date}) => {
-    
+
+    const [ISODate, setISODate] = useState('')
+
+    useEffect(() => {
+        setISODate(backDate)
+    }, [ISODate])
+
+    var backDate = date + `T00:00:00.000Z`;
+    console.log(ISODate)
+
+    const classes = useStyle();
         const history = useHistory();
-    
         const directToDaily = () => {
             let path = '/Daily'
             history.push(path)
         }
-    
+
+
     const userId = localStorage.getItem('userId')
     const [memos, setMemos] = useState([])
-
-    const deleteEntry = () => {
-        console.log(memos[0]._id)
-        API.deleteJournal(memos[0]._id)
-    }
-
     useEffect(() => {
         const loadMemos = async () => {
             const memosFromServer = await API.getJournal(userId)
@@ -38,15 +59,12 @@ const Journal = ({date}) => {
         }
         loadMemos()
     }, [date])
-
-    
     return (
         <>
-            { memos.length > 0 ? <Memos memos={memos} deleteEntry={deleteEntry} /> : <historyEntry onClick={directToDaily}>test</historyEntry>}  
-
+        <dateContext.Provider value={ISODate}>
+            {memos.length > 0 ? <Memos memos={memos} /> : <Button onClick={directToDaily} className={classes.backdateButton}>Add an entry for this day</Button>}  
+        </dateContext.Provider>
         </>
     )
 }
-
 export default Journal
-
